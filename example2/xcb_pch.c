4fd0475fa49a5036a2535dc7d37b99b3b8f44042
Andrzej Borucki <borucki_andrzej@wp.pl>
2019-02-23 13:22:24 +0100

    added pch to myxcb target

---
+++ example2/xcb_pch.c
@@ -0,0 +1,1 @@
+#include "xcb_pch.h"
---
+++ example2/xcb_pch.h
@@ -0,0 +1,8 @@
+#include <inttypes.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <xcb/xcb.h>
